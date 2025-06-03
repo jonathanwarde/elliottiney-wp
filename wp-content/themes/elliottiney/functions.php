03dc346bd6df5bf1a39b63fcab3b793b77da0658
Jon Warde <jon.warde@7dots.co.uk>
2025-06-03 14:11:30 +0100

    add theme support for featured image

--- wp-content/themes/elliottiney/functions.php
+++ wp-content/themes/elliottiney/functions.php
@@ -1,4 +1,8 @@
 <?php
+
+add_action( 'after_setup_theme', function() {
+    add_theme_support( 'post-thumbnails', [ 'post', 'page', 'project' ] );
+});
 // NOTE: no longer attempting next js build on server due to memory issues - now building and deploying locally
 //require_once get_template_directory() . '/functions/nextjsbuildhook.php';
 
